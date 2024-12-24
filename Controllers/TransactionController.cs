e0db6f2b796be0cfa87ad4d121577ceee01bea5a
Muhammad Tarek <mohammadtarek000@gmail.com>
2024-12-24 11:26:55 +0200

    before stopping

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -25,12 +25,27 @@ namespace expense_tracker.Controllers
         public async Task<IActionResult> AddOrEdit(int? id = 0)
         {
             if (_context.Transactions == null) return Problem("Entity set is null");
+
+            PopluateCategories();
             if (id == 0) return View(new Transaction());
 
             Transaction? transaction = await _context.Transactions.FindAsync(id);
             if (transaction == null) return NotFound("Transation not found");
 
             return View(transaction);
+        }
+
+        [NonAction]
+        public void PopluateCategories()
+        {
+            if (_context.Categories == null) return;
+
+            List<Category> categories = _context.Categories.ToList();
+
+            Category defaultCategory = new Category() { Id = 0, Title = "Select Category" };
+            categories.Insert(0, defaultCategory);
+
+            ViewBag.Categories = categories;
         }
     }
 }
